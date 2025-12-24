ae30f0abfcc9a429efcef691b1b534a4477ecde6
lumiere <173829746yzpq@gmail.com>
2025-12-23 20:19:38 -0500

    fixed knockback direction

--- a-n--ice-game/scripts/in-game/sword_swipe.gd
+++ a-n--ice-game/scripts/in-game/sword_swipe.gd
@@ -3,7 +3,8 @@ extends Area2D
 class_name SwordSwipe
 @export var animation: AnimatedSprite2D
 @export var attack_distance: float
-var facing: FacingComponent.Facing = FacingComponent.Facing.FORWARD
+
+var direction_vector: Vector2
 
 func _ready() -> void:
 	position = Vector2.ZERO
@@ -13,32 +14,10 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node) -> void:
 	if body is Enemy:
-		body.get_hit(1, FacingComponent.facing_direction_dictionary[facing])
-
-#func swipe(new_facing: FacingComponent.Facing) -> void:
-	#var offset: Vector2
-	#match new_facing:
-		#FacingComponent.Facing.LEFT:
-			#offset = Vector2(-attack_distance, 0)
-			#rotation = PI / 2
-		#FacingComponent.Facing.RIGHT:
-			#offset = Vector2(attack_distance, 0)
-			#rotation = -PI / 2
-		#FacingComponent.Facing.BACKWARD:
-			#offset = Vector2(0, -attack_distance)
-			#rotation = PI
-		#FacingComponent.Facing.FORWARD:
-			#offset = Vector2(0, attack_distance)
-			#rotation = 0
-	#position = offset
-	#
-	#show()
-	#facing = new_facing
-	#monitoring = true
-	#animation.play(&"default")
+		body.get_hit(1, direction_vector)
 
 func swipe() -> void:
-	var direction_vector: Vector2 = (get_global_mouse_position() - global_position).normalized()
+	direction_vector = (get_global_mouse_position() - global_position).normalized()
 	
 	# prevent NaNs if mouse is exactly on top of us
 	if direction_vector.length_squared() < 0.000001:
