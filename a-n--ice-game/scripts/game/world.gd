1a454861f13fbdee2836b9388fb61ce776c70219
lumiere <173829746yzpq@gmail.com>
2025-12-25 05:47:10 -0500

    feat/first area

--- a-n--ice-game/scripts/game/world.gd
+++ a-n--ice-game/scripts/game/world.gd
@@ -2,9 +2,13 @@ extends Node2D
 
 class_name World
 
+@export var spawnpoint: Node2D
+
 var player: Player
 
-func setup(new_player: Player, player_spawn_point: Vector2):
+func setup(new_player: Player, _spawnpoint_index: int):
 	player = new_player
 	player.reparent(self)
-	player.position = player_spawn_point
+	player.position = spawnpoint.position
+	
+	spawnpoint.get_child(0).hide()
