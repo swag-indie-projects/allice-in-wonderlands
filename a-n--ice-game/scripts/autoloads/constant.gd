c6aab03e164c32ba79082ff3bcca2abfaff9d81e
2192375d <173829746yzpq@gmail.com>
2025-12-24 20:43:59 -0500

    feat: pause menu

--- a-n--ice-game/scripts/autoloads/constant.gd
+++ a-n--ice-game/scripts/autoloads/constant.gd
@@ -1,3 +1,7 @@
 extends Node
 
 const PLAYER_STARTING_HP: int = 5
+
+# I have to include this because otherwise I will be creating circular dependency.
+# So from now on, everytime when the path is changed you have to manually change it here
+const PATH_TO_MAIN_MENU: String = "res://scenes/main_menu.tscn"
--- a-n--ice-game/scripts/main menu.gd
+++ a-n--ice-game/scripts/main menu.gd
@@ -4,6 +4,7 @@ extends Node2D
 
 func _on_start_game_pressed() -> void:
 	get_tree().change_scene_to_packed(game_scene)
+	
 
 
 func _on_exit_pressed() -> void:
