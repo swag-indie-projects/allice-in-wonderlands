b76c500cfc41512c915954d3cb25f3c930cebe6e
2192375d <173829746yzpq@gmail.com>
2025-12-25 22:25:54 -0500

    feat: room transition done!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- a-n--ice-game/scripts/game/world.gd
+++ a-n--ice-game/scripts/game/world.gd
@@ -2,13 +2,29 @@ extends Node2D
 
 class_name World
 
-@export var spawnpoint: Node2D
+@export var spawnpoints: Array[Node2D]
+@export var exitpoint_to_scene: Dictionary[Area2D, SpawnResult]
 
 var player: Player
+signal exited(result: SpawnResult)
 
-func setup(new_player: Player, _spawnpoint_index: int):
+func setup(new_player: Player, spawnpoint_index: int):
+	
+	if spawnpoint_index >= spawnpoints.size():
+		printerr("World: function setup: Invalid spawanpoint_index")
+	
 	player = new_player
 	player.reparent(self)
-	player.position = spawnpoint.position
+	player.position = spawnpoints[spawnpoint_index].position
 	
-	spawnpoint.get_child(0).hide()
+	for spawnpoint: Node2D in spawnpoints:
+		spawnpoint.hide()
+	for exitpoint: Area2D in exitpoint_to_scene.keys():
+		exitpoint.body_entered.connect(_on_exitpoint_body_entered.bind(exitpoint))
+		exitpoint.hide()
+
+func _on_exitpoint_body_entered(body: Node2D, exitpoint: Area2D) -> void:
+	print("Here")
+	if !(body is Player):
+		return
+	exited.emit(exitpoint_to_scene[exitpoint])
