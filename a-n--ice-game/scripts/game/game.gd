97a7112fef1a38783782fbee153f715675706475
yoshixi <davidchenyuhe@proton.me>
2025-12-27 13:09:48 -0500

    feat: save system

--- a-n--ice-game/scripts/game/game.gd
+++ a-n--ice-game/scripts/game/game.gd
@@ -5,13 +5,26 @@ extends Node2D
 @export var test_world_scene_path: Constant.Paths = Constant.Paths.PATH_TO_TEST_SCENE
 @export var player_healthbar_ui: PlayerHealthbarUI
 
+@onready var game_state : GameState = $GameState
 @onready var current_world: World = null
+
+signal coin_change(val);
 
 func _ready() -> void:
 	player.HP_changed.connect(_on_player_HP_changed)
 	
 	play_world(load(Constant.path_to_string[starting_world_scene_path]), 0)
 	Globals.game = self
+	
+	game_state.load_config() # load game
+	get_tree().auto_accept_quit = false # prevent auto close without saving
+
+func _notification(event: int) -> void:
+	if event == NOTIFICATION_WM_CLOSE_REQUEST:
+		# do save stuff here
+		game_state.save_config()
+		# quit game
+		get_tree().quit()
 
 func play_world(scene: PackedScene, spawn_point_index: int) -> void:
 	if is_instance_valid(current_world):
@@ -24,11 +37,10 @@ func play_world(scene: PackedScene, spawn_point_index: int) -> void:
 	current_world.setup(player, spawn_point_index)
 	add_child.call_deferred(current_world)
 
-
-
 func _on_world_exited(result: SpawnResult) -> void:
 	var target_scene: PackedScene = load(Constant.path_to_string[result.scene_path])
 	play_world(target_scene, result.spawnpoint_index)
 
 func _on_player_HP_changed(HP: int, max_HP: int):
 	player_healthbar_ui.update_healthbar.emit(HP, max_HP)
+	coin_change.emit(1)
