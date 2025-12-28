ff912ace7ecd0be9c6330cea216ba0bc76223e76
yoshixi <davidchenyuhe@proton.me>
2025-12-28 11:56:40 -0500

    fix: fixing the ui overlay bug

--- a-n--ice-game/scripts/game/ui/save_popup.gd
+++ a-n--ice-game/scripts/game/ui/save_popup.gd
@@ -5,11 +5,13 @@ class_name SavePopupUI
 
 func show_save_popup() -> void:
 	print("save popup...")
+	self.visible = true
 	panel.visible = true
 	while panel.modulate.a >= 0:
 		await get_tree().create_timer(1.0).timeout
 		panel.modulate.a -= 0.10
-	panel.visible = false
+	
+	self.visible = false
 	panel.modulate.a = 1.0
 
 func _ready():
