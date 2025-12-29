769fd3f9597c83280dd38265771fb1e8682d27ad
Kido <63456333+the-kido@users.noreply.github.com>
2025-12-29 18:27:35 -0500

    feat: Added death screen

--- a-n--ice-game/scripts/game/player/state_machine/states/player/player_hit_state.gd
+++ a-n--ice-game/scripts/game/player/state_machine/states/player/player_hit_state.gd
@@ -10,9 +10,15 @@ var direction_vector: Vector2
 
 func enter() -> void:
 	super()
+	
 	actor.get_hit_ui.get_hit.emit()
 	actor.camera.shaking.emit()
 	
+	print(actor.HP)
+	if actor.HP == 0:
+		actor.death_menu.on_die.emit()
+		return
+
 	time_elapsed = 0.0
 	acceleration = 2 * knockback / knockback_duration / knockback_duration
 	direction_vector = FacingComponent.facing_direction_dictionary[FacingComponent.get_reverse_facing(actor.facing_component.facing)]
