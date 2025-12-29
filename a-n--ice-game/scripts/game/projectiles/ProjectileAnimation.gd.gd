fad7c232260f043d8b628e5d03a0ba58dc7d9857
Kido <63456333+the-kido@users.noreply.github.com>
2025-12-29 10:20:12 -0500

    feat: Fixed annoying warnings and added sign + tent

--- a-n--ice-game/scripts/game/projectiles/ProjectileAnimation.gd.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-class_name ProjectileAnimation
-
-func _summon_projectiles(world: World, target_position: Vector2, spawn_position: Vector2):
-	pass
--- a-n--ice-game/scripts/game/projectiles/projectile_animation.gd
+++ a-n--ice-game/scripts/game/projectiles/projectile_animation.gd
@@ -2,5 +2,5 @@ extends Node
 
 class_name ProjectileAnimation
 
-func _summon_projectiles(world: World, direction: Vector2, spawn_point: Vector2):
+func _summon_projectiles(world: World, target_position: Vector2, spawn_position: Vector2):
 	pass
