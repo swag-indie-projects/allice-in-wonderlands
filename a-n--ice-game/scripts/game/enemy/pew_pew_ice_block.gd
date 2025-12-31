da15c9fde8cae6bc8ded173538b478f598973d28
2192375d <173829746yzpq@gmail.com>
2025-12-31 18:58:54 -0500

    feat: another a lot of things

--- a-n--ice-game/scripts/game/enemy/pew_pew_ice_block.gd
+++ a-n--ice-game/scripts/game/enemy/pew_pew_ice_block.gd
@@ -8,8 +8,13 @@ extends Enemy
 @export var bullet_speed: float
 @export var bullet_direction: float
 
+@export var shoot_timer_interval: float
+@export var wait_timer_interval: float
+@export var num_shot_before_wait: int
+
 var world: World
 var dying: bool = false
+@onready var num_shot: int = 0
 
 func _ready() -> void:
 	if !get_parent() is World:
@@ -18,7 +23,10 @@ func _ready() -> void:
 	
 	if health_bar != null:
 		health_bar.max_value = MAX_HP
-	rotate(bullet_direction)
+	rotation = bullet_direction
+	
+	$shoot_timer.wait_time = shoot_timer_interval
+	$wait_timer.wait_time = wait_timer_interval
 
 func _physics_process(delta: float) -> void:
 	super(delta)
@@ -65,8 +73,18 @@ func get_hit(amount: int, direction_vector: Vector2) -> void:
 	if knockback_component != null:
 		knockback_component.setup(direction_vector)
 
-func _on_timer_timeout() -> void:
+func _on_shoot_timer_timeout() -> void:
+	if num_shot >= num_shot_before_wait && num_shot_before_wait > 0:
+		$shoot_timer.stop()
+		$wait_timer.start(wait_timer_interval)
+		return
+	
 	var bullet: SnowballProjectile = bullet_scene.instantiate()
 	bullet.global_position = global_position
 	bullet._set_data(Vector2(cos(bullet_direction), sin(bullet_direction)), bullet_speed)
 	get_parent().add_child(bullet)
+	num_shot += 1
+
+func _on_wait_timer_timeout() -> void:
+	num_shot = 0
+	$shoot_timer.start(shoot_timer_interval)
