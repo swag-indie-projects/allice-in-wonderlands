a4b8a75fc101fa16417f9a167c46dc724841a610
Kido <63456333+the-kido@users.noreply.github.com>
2025-12-31 14:04:37 -0500

    feat: Added a nicer method to reuse fridge + smoother animations

--- a-n--ice-game/scripts/game/ui/save_popup.gd
+++ a-n--ice-game/scripts/game/ui/save_popup.gd
@@ -4,15 +4,5 @@ class_name SavePopupUI
 @onready var panel : PanelContainer = $PanelContainer
 
 func show_save_popup() -> void:
-	print("save popup...")
-	self.visible = true
-	panel.visible = true
-	while panel.modulate.a >= 0:
-		await get_tree().create_timer(1.0).timeout
-		panel.modulate.a -= 0.10
-	
-	self.visible = false
-	panel.modulate.a = 1.0
-
-func _ready():
-	pass;
+	$AnimationPlayer.stop()
+	$AnimationPlayer.play("show")
