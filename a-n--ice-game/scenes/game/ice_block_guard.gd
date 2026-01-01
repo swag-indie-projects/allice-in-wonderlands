8f7ee5e69d7e5ea68ef810df17ef3a50399d5e52
Kido <63456333+the-kido@users.noreply.github.com>
2026-01-01 15:30:23 -0500

    feat: cool down sound + lots of fixes

--- a-n--ice-game/scenes/game/ice_block_guard.gd
+++ a-n--ice-game/scenes/game/ice_block_guard.gd
@@ -1,11 +1,9 @@
 extends Area2D
 
-
 var duplicated_shape: CollisionShape2D
 @export var ice_float: IceFloat
 
 func _ready() -> void:
-	print(ice_float.base_overlay_tilemap)
 	# Get the collision shape child
 	var collision_shape: CollisionShape2D = $CollisionShape2D
 	
@@ -17,13 +15,10 @@ func _ready() -> void:
 	var number = area_name.replace("Area2D", "")
 	duplicated_shape.name = number
 	
-	
 	# Add to parent (StaticBody2D)
 	var parent = get_parent()
 	if parent is StaticBody2D:
 		parent.add_child.call_deferred(duplicated_shape)
-	else:
-		push_error("Parent is not a StaticBody2D")
 
 func _process(delta: float) -> void:
 	# Check if overlapping with any other Area2D
@@ -34,9 +29,4 @@ func _process(delta: float) -> void:
 	
 	var overlapping_areas = get_overlapping_areas()
 	
-	if overlapping_areas.size() > 0:
-		print(overlapping_areas[0].name, "overlapping ", name)
-		print("hello?")
-		duplicated_shape.disabled = true
-	else:
-		duplicated_shape.disabled = false
+	duplicated_shape.disabled = overlapping_areas.size() > 0
--- a-n--ice-game/scripts/game/projectiles/projectile_animation.gd
+++ a-n--ice-game/scripts/game/projectiles/projectile_animation.gd
@@ -1,6 +1,5 @@
+@abstract class_name ProjectileAnimation
+
 extends Node
 
-class_name ProjectileAnimation
-
-func _summon_projectiles(world: World, target_position: Vector2, spawn_position: Vector2):
-	pass
+@abstract func _summon_projectiles(world: World, target_position: Vector2, spawn_position: Vector2)
